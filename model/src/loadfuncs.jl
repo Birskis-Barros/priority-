6a00bf390093242d6811b5dd5ee4faabc1f60dfd
Mac <mac@ElCapitan-a820662d7994.local>
2018-11-27 19:46:34 -0800

    Arrumando o homedir()

--- model/src/loadfuncs.jl
+++ model/src/loadfuncs.jl
@@ -14,22 +14,23 @@ using DataFrames
 # @everywhere using HDF5
 @everywhere using JLD
 
-if homedir() == "/home/z840"
-    
-    
+if homedir() == "/home/irinabarros"
+
+#deu certo? 
+
     #Interaction matrix
-    @everywhere include("$(homedir())/2018_discrete_enigma/model/src/intmatrixv3.jl")
-    @everywhere include("$(homedir())/2018_discrete_enigma/model/src/preamble_defs.jl")
-    @everywhere include("$(homedir())/2018_discrete_enigma/model/src/assemblystate.jl")
-    @everywhere include("$(homedir())/2018_discrete_enigma/model/src/potcol.jl")
-    
-    
+    @everywhere include("$(homedir())/Documents/src/intmatrixv3.jl")
+    @everywhere include("$(homedir())/Documents/src/preamble_defs.jl")
+    @everywhere include("$(homedir())/Documents/src/assemblystate.jl")
+    @everywhere include("$(homedir())/Documents/src/potcol.jl")
+
+
 else
-    
-    
+
+
     #Interaction matrix
-    @everywhere include("$(homedir())/Dropbox/PostDoc/2018_discrete_enigma/model/src/intmatrixv3.jl")
-    @everywhere include("$(homedir())/Dropbox/PostDoc/2018_discrete_enigma/model/src/preamble_defs.jl")
-    @everywhere include("$(homedir())/Dropbox/PostDoc/2018_discrete_enigma/model/src/assemblystate.jl")
-    @everywhere include("$(homedir())/Dropbox/PostDoc/2018_discrete_enigma/model/src/potcol.jl")
+    @everywhere include("$(homedir())/Dropbox/2018_discrete_enigma/model/src/intmatrixv3.jl")
+    @everywhere include("$(homedir())/Dropbox/2018_discrete_enigma/model/src/preamble_defs.jl")
+    @everywhere include("$(homedir())/Dropbox/2018_discrete_enigma/model/src/assemblystate.jl")
+    @everywhere include("$(homedir())/Dropbox/2018_discrete_enigma/model/src/potcol.jl")
 end
